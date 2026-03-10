a52b3f1d2cacde1b6fd8b054ae31b455ab6e504e
csj4real <150768817+csj4real@users.noreply.github.com>
2026-03-10 12:01:17 -0400

    new api key

--- src/java/Controller/ConvertServlet.java
+++ src/java/Controller/ConvertServlet.java
@@ -29,7 +29,7 @@ import java.util.concurrent.CompletableFuture;
 @MultipartConfig
 public class ConvertServlet extends HttpServlet {
 
-    private static final String API_KEY = "n5RQPGiyeqfFZ0T3RkKStZjad3wXG6he";
+    private static final String API_KEY = "ObmahfkGldwx98BLbxjHHpXHPk6gjjbB";
     private static final String UPLOAD_DIR = "uploads";
     private static final String CONVERTED_DIR = "converted_files";
 
